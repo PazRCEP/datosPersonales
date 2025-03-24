781fc04bf280b019720792475621eefc92e5aced
Paz Rueda Politècnics <102384121+PazRCEP@users.noreply.github.com>
2025-03-24 15:43:08 +0100

    cambio1

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
         System.out.println("hola");
         System.out.println("hola");
+        System.out.println("hola");
         
         try {
             // Solicitar datos personales al usuario
